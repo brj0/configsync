83b58984cc0f06fea58d6094e1759e2783d0a215
brj0 <jon.brugger@sunrise.ch>
2025-02-24 22:14:57 +0100

    New backup 2025-02-24 22:14:57

--- config/.config/nvim/lua/plugins/misc.lua
+++ config/.config/nvim/lua/plugins/misc.lua
@@ -9,6 +9,18 @@ return {
         }
     },
 
+    -- Add/delete/change surrounding pairs
+    {
+        "kylechui/nvim-surround",
+        version = "*", -- Use for stability; omit to use `main` branch for the latest features
+        event = "VeryLazy",
+        config = function()
+            require("nvim-surround").setup({
+                -- Configuration here, or leave empty to use defaults
+            })
+        end
+    },
+
     -- Highlight todo, notes, etc in comments:
     -- TODO:
     -- NOTE:
