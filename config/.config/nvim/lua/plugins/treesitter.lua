33ef8edda2cdf257837188a4e275fb70d9258705
brj0 <jon.brugger@sunrise.ch>
2025-03-27 22:21:32 +0100

    New backup 2025-03-27 22:21:32

--- config/.config/nvim/lua/plugins/treesitter.lua
+++ config/.config/nvim/lua/plugins/treesitter.lua
@@ -2,13 +2,25 @@ return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function () 
-    local configs = require("nvim-treesitter.configs")
+        local configs = require("nvim-treesitter.configs")
 
-    configs.setup({
-        ensure_installed = { "c", "cpp", "css", "elixir", "heex", "html", "javascript", "lua", "python", "query", "rust", "vim", "vimdoc" },
-        sync_install = false,
-        highlight = { enable = true },
-        indent = { enable = true },  
+        configs.setup({
+            ensure_installed = { "c", "cpp", "css", "elixir", "heex", "html", "javascript", "lua", "python", "query", "rust", "vim", "vimdoc" },
+            sync_install = false,
+            highlight = { enable = true },
+            indent = {
+                enable = true,
+                disable = { "python" },
+            },
         })
+
+        -- Prevent over-indentation in Python
+        vim.api.nvim_create_autocmd("FileType", {
+            pattern = "python",
+            callback = function()
+                vim.bo.indentexpr = ""
+            end,
+        })
+
     end
 }
