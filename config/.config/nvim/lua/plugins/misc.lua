2c7fc3c9d95e8d3fc707b5ab54f0ebe432502877
brj0 <jon.brugger@sunrise.ch>
2026-01-28 21:23:29 +0100

    New backup 2026-01-28 21:23:29

--- config/.config/nvim/lua/plugins/misc.lua
+++ config/.config/nvim/lua/plugins/misc.lua
@@ -67,4 +67,13 @@ return {
         "ShaiberAlon/snakemake-vim"
     },
 
+    -- File browser
+    {
+        'stevearc/oil.nvim',
+        opts = {},
+        dependencies = { { "nvim-mini/mini.icons", opts = {} } },
+        -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+        lazy = false,
+    },
+
 }
